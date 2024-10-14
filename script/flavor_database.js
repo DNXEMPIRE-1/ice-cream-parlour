cddf5fa4cc26706ae5c25a80d9a2278af21e2f64
“Dennis <dennisnzioki019@gmail.com>
2024-10-14 22:17:28 +0300

    Create flavor_database.js

---
+++ script/flavor_database.js
@@ -0,0 +1,24 @@
+const falvors_list = [
+    {
+        id: "f001",
+        name: "Regular"
+    },
+    {
+        id: "f002",
+        name: "Gelato"
+    },
+    {
+        id: "f003",
+        name: "Sorbet"
+    },
+    {
+        id: "f004",
+        name: "Frozen Yogurt"
+    },
+    {
+        id: "f005",
+        name: "Non Diary/Vegen"
+    }
+]
+
+export default falvors_list;
